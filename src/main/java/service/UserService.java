2369148da8a00f05d107dde790bd32c63c17228e
mehdi <mehdiarghan@gmail.com.git/>
2023-10-16 21:49:39 +0330

    Implementation of the findByUserName method in UserServiceImpl and UserService classes

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,5 +3,8 @@ package service;
 import base.service.BaseService;
 import entity.User;
 
+import java.util.Optional;
+
 public interface UserService extends BaseService<Long, User> {
+    Optional<User> findByUserName(String UserName);
 }
--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -6,8 +6,14 @@ import org.hibernate.Session;
 import repository.UserRepository;
 import service.UserService;
 
+import java.util.Optional;
+
 public class UserServiceImpl extends BaseServiceImpl<Long, User, UserRepository> implements UserService {
     public UserServiceImpl(Session session, UserRepository repository) {
         super(session, repository);
+    }
+    @Override
+    public Optional<User> findByUserName(String UserName) {
+        return repository.findByUserName(UserName);
     }
 }
